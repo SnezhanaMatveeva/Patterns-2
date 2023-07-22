6ad86bc8158c019579e07e9684332d576c48a07c
Viktoriia <vick2021@mail.com>
2023-07-21 22:29:53 -0700

    add new logic

--- src/test/java/ru/netology/testmode/AuthTest.java
+++ src/test/java/ru/netology/testmode/AuthTest.java
@@ -25,7 +25,7 @@ class AuthTest {
         var registeredUser = getRegisteredUser("active");
         $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
         $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
-        $("button.button").click()
+        $("button.button").click();
         $("h2").shouldHave(Condition.exactText("Личный кабинет")).shouldBe(Condition.visible);
     }
 
